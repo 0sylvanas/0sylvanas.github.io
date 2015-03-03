9a4263a0a223d0f889e82b21b875419f3e6640c2
Lee <haoyang.li@dianping.com>
2015-03-03 15:05:55 +0800

    rotate&clock

---
+++ rotate-clock/main.js
@@ -0,0 +1,36 @@
+/* 
+ * @Author: haoyang.li
+ * @Date:   2015-03-03 12:05:41
+ * @Last Modified by:   haoyang.li
+ * @Last Modified time: 2015-03-03 14:58:07
+ */
+	setInterval(function() {
+		var seconds = new Date().getSeconds();
+		var sdegree = seconds * 6;
+		var srotate = "rotate(" + sdegree + "deg)";
+		$("#sec").css({
+			"-moz-transform": srotate,
+			"-webkit-transform": srotate
+		});
+	}, 1000);
+	setInterval(function() {
+		var hours = new Date().getHours();
+		var mins = new Date().getMinutes();
+		var hdegree = hours * 30 + (mins / 2);
+		var hrotate = "rotate(" + hdegree + "deg)";
+		$("#hour").css({
+			"-moz-transform": hrotate,
+			"-webkit-transform": hrotate
+		});
+	}, 1000);
+	setInterval(function() {
+		var mins = new Date().getMinutes();
+		var mdegree = mins * 6;
+		var mrotate = "rotate(" + mdegree + "deg)";
+
+		$("#min").css({
+			"-moz-transform": mrotate,
+			"-webkit-transform": mrotate
+		});
+
+	}, 1000);
